f8747aca4de8ca90c490ad09ef97521df4ca836b
Oliver Dozsa <dozsa.oliver@gmail.com>
2023-03-28 21:19:31 +0200

    Adding tailwind and some initial content.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppRoutes} from "./app-routes";
+import {HomeComponent} from "./pages/home/home.component";
+import {SignupComponent} from "./pages/signup/signup.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: `/${AppRoutes.HOME}`, pathMatch: 'full' },
+  {path: AppRoutes.HOME, component: HomeComponent},
+  {path: AppRoutes.SIGNUP, component: SignupComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AppRoutes} from "./app-routes";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  AppRoutes = AppRoutes;
+
   title = 'galactic-pub';
 }
