5bb11804c83b3734b8ad5ec4520fb1ec95220d98
Oliver Dozsa <dozsa.oliver@gmail.com>
2023-06-09 22:10:39 +0200

    Updated server URL.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api-vote.galactic.pub',
+  apiUrl: 'https://host.galactic.pub',
   maxVotesLimit: 200,
   stellarNumOfVoteBuckets: 5
 };
