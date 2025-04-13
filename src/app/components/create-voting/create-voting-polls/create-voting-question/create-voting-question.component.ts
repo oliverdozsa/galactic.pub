2eb5438ce2d7747da953cc77044b38dacab46c8a
Oliver Dozsa <dozsa.oliver@gmail.com>
2025-04-13 21:07:37 +0200

    Stared poll question and description validation.

--- src/app/components/create-voting/create-voting-polls/create-voting-question/create-voting-question.component.ts
+++ src/app/components/create-voting/create-voting-polls/create-voting-question/create-voting-question.component.ts
@@ -1,14 +1,35 @@
 import {Component, Input} from '@angular/core';
 import {CreateVotingRequest} from '../../create-voting-request';
 import {CreatePollRequest} from '../../create-poll-request';
+import {NgIf} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-create-voting-question',
-  imports: [],
+  imports: [
+    NgIf,
+    FormsModule
+  ],
   templateUrl: './create-voting-question.component.html',
   styleUrl: './create-voting-question.component.css'
 })
 export class CreateVotingQuestionComponent {
   @Input()
   pollRequest!: CreatePollRequest;
+
+  get isQuestionValid() {
+    // TODO
+    const question = this.pollRequest.question;
+    return question && question.length >= 2 && question.length <= 1000;
+  }
+
+  get isDescriptionValid() {
+    const description = this.pollRequest.description;
+
+    if (description) {
+      return description.length <= 1000;
+    }
+
+    return true;
+  }
 }
