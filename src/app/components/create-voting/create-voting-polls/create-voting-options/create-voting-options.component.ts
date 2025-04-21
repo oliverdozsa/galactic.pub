d1b2b7b38c103645ecab54c9012ef8f856bfa443
Oliver Dozsa <dozsa.oliver@gmail.com>
2025-04-21 21:42:29 +0200

    Adding poll option handling.

--- src/app/components/create-voting/create-voting-polls/create-voting-options/create-voting-options.component.ts
+++ src/app/components/create-voting/create-voting-polls/create-voting-options/create-voting-options.component.ts
@@ -1,13 +1,28 @@
 import {Component, Input} from '@angular/core';
 import {CreatePollRequest} from '../../create-poll-request';
+import {NgForOf, NgIf} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {CreatePollOptionRequest} from '../../create-poll-option-request';
 
 @Component({
   selector: 'app-create-voting-options',
-  imports: [],
+  imports: [
+    NgForOf,
+    FormsModule,
+    NgIf
+  ],
   templateUrl: './create-voting-options.component.html',
   styleUrl: './create-voting-options.component.css'
 })
 export class CreateVotingOptionsComponent {
   @Input()
   pollRequest!: CreatePollRequest;
+
+  onAddOptionClicked() {
+    this.pollRequest.options.push({name: "", code: this.pollRequest.options.length + 1});
+  }
+
+  isOptionNameValid(option: CreatePollOptionRequest) {
+    return option.name.length >= 2 && option.name.length <= 300;
+  }
 }
