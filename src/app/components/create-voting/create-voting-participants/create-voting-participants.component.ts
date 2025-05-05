36818caab0814e645fffbaaf9752d3267e0fdeb7
Oliver Dozsa <dozsa.oliver@gmail.com>
2025-05-05 21:03:39 +0200

    Adding participants.

--- src/app/components/create-voting/create-voting-participants/create-voting-participants.component.ts
+++ src/app/components/create-voting/create-voting-participants/create-voting-participants.component.ts
@@ -1,13 +1,40 @@
 import {Component, Input} from '@angular/core';
 import {CreateVotingRequest} from '../create-voting-request';
+import {FormsModule} from '@angular/forms';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-create-voting-participants',
-  imports: [],
+  imports: [
+    FormsModule,
+    NgForOf
+  ],
   templateUrl: './create-voting-participants.component.html',
   styleUrl: './create-voting-participants.component.css'
 })
 export class CreateVotingParticipantsComponent {
   @Input()
   votingRequest!: CreateVotingRequest;
+
+  participants: string[] = [];
+
+  emailInput = "";
+
+  private emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+  get isEmailValid() {
+    return this.emailRegex.test(this.emailInput);
+  }
+
+  onAddEvent() {
+    if (this.isEmailValid) {
+      this.participants.push(this.emailInput)
+    }
+
+    this.emailInput = "";
+  }
+
+  onDelete(index: number) {
+    this.participants.splice(index, 1);
+  }
 }
