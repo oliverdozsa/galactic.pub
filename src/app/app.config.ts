5e4348518b5bef75d79e7f09c16b5b1f0cc20f51
Oliver Dozsa <dozsa.oliver@gmail.com>
2025-06-22 23:11:11 +0200

    Fixing DI based auth interceptor usage from lib.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,9 @@ import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
-import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import {OAuthStorage, provideOAuthClient} from 'angular-oauth2-oidc';
 import {environment} from '../environments/environment';
-import {authenticationInterceptor} from './interceptors/authentication.interceptor';
 
 export function storageFactory(): OAuthStorage {
   return localStorage
@@ -15,8 +14,8 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([])),
-    provideOAuthClient({resourceServer: {allowedUrls: [environment.apiUrl, "http://localhostq"], sendAccessToken: true}}),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideOAuthClient({resourceServer: {allowedUrls: [environment.apiUrl, "localhost"], sendAccessToken: true}}),
     {provide: OAuthStorage, useFactory: storageFactory}
   ]
 };
