ae4cd53304f02f14c78b4edb32d319b1f52b0f68
Oliver Dozsa <dozsa.oliver@gmail.com>
2025-08-01 16:07:15 +0200

    Reorganizing view voting.

---
+++ src/app/components/view-voting/basic-data/basic-data.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-data',
+  imports: [],
+  templateUrl: './basic-data.component.html',
+  styleUrl: './basic-data.component.css'
+})
+export class BasicDataComponent {
+
+}
