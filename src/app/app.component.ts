2b6f80dc451d33bf49bd68800555f0295c15a39c
Oliver Dozsa <dozsa.oliver@gmail.com>
2026-06-07 17:38:01 +0200

    Added about section.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,18 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { AboutComponent } from './about/about.component';
 import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, HeroComponent, ProjectsComponent, FooterComponent],
+  imports: [HeaderComponent, HeroComponent, ProjectsComponent, AboutComponent, FooterComponent],
   template: `
     <app-header />
     <main>
       <app-hero />
       <app-projects />
+      <app-about />
     </main>
     <app-footer />
   `,
